47d8c3eace27a686d58959d154a10b2662860f4d
Vasco Martins <vascomartins37@gmail.com>
2021-07-05 20:15:06 +0100

    Update deploy

--- stubs/setup.sh
+++ stubs/setup.sh
@@ -8,5 +8,5 @@ php artisan route:clear
 php artisan route:cache
 php artisan migrate:fresh --seed
 
-ln -s #--SOURCE_FILE--# #--SYMBOLIC-LINK--#
+ln -s #--SOURCE_FILE--#/public #--SYMBOLIC-LINK--#
 sudo chown -R $USER:apache ~#--SOURCE_FILE--#

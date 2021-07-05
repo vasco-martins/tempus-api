ece9390191037b3696a152e4563c1ffee039b06f
Vasco Martins <vascomartins37@gmail.com>
2021-07-05 22:39:45 +0100

    Update deploy

--- app/Jobs/DeployProjectJob.php
+++ app/Jobs/DeployProjectJob.php
@@ -99,7 +99,7 @@ class DeployProjectJob implements ShouldQueue
         // 4 - A instalar os ficheiros de configuração
         $this->project->update(['deploy_status' => 5]);
 
-        $this->runCommandWait(['php', 'composer.phar', 'install']);
+        $this->runCommandWait(['yes', '|', 'php', 'composer.phar', 'install']);
 
 
 

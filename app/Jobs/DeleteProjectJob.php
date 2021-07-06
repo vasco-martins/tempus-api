c84a6c06c8c24f076f8c56e07c145fba83b84d33
Vasco Martins <vascomartins37@gmail.com>
2021-07-06 08:40:34 +0100

    Delete project

--- app/Jobs/DeleteProjectJob.php
+++ app/Jobs/DeleteProjectJob.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Symfony\Component\Process\Process;
 
 class DeleteProjectJob implements ShouldQueue
 {
@@ -35,8 +36,14 @@ class DeleteProjectJob implements ShouldQueue
     public function handle()
     {
         if(is_dir($this->project->folder)) {
-            $this->delTree($this->project->folder);
+            $this->runCommandWait(['sudo', 'rm', '-rf',  $this->project->folder], '/home/');
         }
+
+        if(is_dir(config('app.project_deploy_path') . $this->project->slug)) {
+            $this->runCommandWait(['sudo', 'rm', '-rf',  config('app.project_deploy_path') . $this->project->slug], '/home/');
+        }
+
+
     }
 
     private function delTree($dir): bool
@@ -49,4 +56,18 @@ class DeleteProjectJob implements ShouldQueue
         return rmdir($dir);
     }
 
+    private function runCommandWait(array $command, $path = null)
+    {
+        if($path == null) $path = $this->path;
+
+        $gitInit = new Process($command);
+        $gitInit->setWorkingDirectory($path);
+
+        $gitInit->run();
+        $gitInit->wait();
+        \Illuminate\Support\Facades\Log::debug(
+            $gitInit->getErrorOutput());
+        \Illuminate\Support\Facades\Log::debug(
+            $gitInit->getOutput());
+    }
 }

c566f843c601677875e9c4155a45773fb0c1bf1f
Vasco Martins <vascomartins37@gmail.com>
2021-07-07 08:24:16 +0100

    Add translations

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'A tua password foi alterada!',
+    'sent' => 'Foi enviado um email com o link para alterares a password!',
+    'token' => 'Token inválido.',
+    'user' => "Não conseguimos encontrar um utilizador com esse email.",
+
+
+];
